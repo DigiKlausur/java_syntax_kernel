d54a90c43cacecfb338ffe28c830bba2e7751fa2
Tim Metzler <tim.metzler@h-brs.de>
2022-05-19 14:37:33 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from distutils.core import setup
 
-with open("README.rst") as f:
-    readme = f.read()
-
 setup(
     name="java_syntax_kernel",
     version="0.1",
     packages=["java_syntax_kernel"],
     description="Restricted python kernel for Jupyter",
-    long_description=readme,
     author="Tim Metzler",
     author_email="tim.metzler@h-brs.de",
     url="https://github.com/DigiKlausur/java_syntax_kernel",
